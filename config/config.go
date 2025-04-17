c935e39588dc6e91746326599bfdd8e6807a7205
kit <1073618744@qq.com>
2025-04-17 20:26:21 +0800

    forked from BitgetLimited/v3-bitget-api-sdk

---
+++ config/config.go
@@ -0,0 +1,14 @@
+package config
+
+import "github.com/339-Labs/v3-bitget-api-sdk-go/constants"
+
+const (
+	BaseUrl = "https://api.bitget.com"
+	WsUrl   = "wss://ws.bitget.com/mix/v1/stream"
+
+	ApiKey        = ""
+	SecretKey     = ""
+	PASSPHRASE    = ""
+	TimeoutSecond = 30
+	SignType      = constants.SHA256
+)
